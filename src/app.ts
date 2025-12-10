fa680f8751b944f24c3a4ce881a06de0d3976233
mohabbatali <mohabbatali@iciclecorporation.com>
2025-12-10 14:08:24 +0600

    origin change

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,6 @@ app.use(cors({
 }));
 // Set custom headers for CORS
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "https://actor-equity.vercel.app/"); // Replace with your frontend domain
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend domain
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");

31f90fe060ec088bd9eca1ab6439f680c9413477
mohabbatali <mohabbatali@iciclecorporation.com>
2025-12-13 12:53:02 +0600

    actor schema updated add all actor deleted api

--- dist/admin/admin.router.js
+++ dist/admin/admin.router.js
@@ -6,10 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const admin_controller_1 = require("./admin.controller");
 const fileUpload_1 = require("../helper/fileUpload");
+const actor_schema_1 = __importDefault(require("../actor/actor.schema"));
 const adminRouter = express_1.default.Router();
 adminRouter.post("/", admin_controller_1.AdminController.createAdmin);
 adminRouter.post("/add-actor", fileUpload_1.fileUploader.upload.single("photo"), admin_controller_1.AdminController.addActor);
 adminRouter.put("/:id", admin_controller_1.AdminController.updateActorProfile);
 adminRouter.get("/", admin_controller_1.AdminController.getAdmin);
 adminRouter.put("/:id", admin_controller_1.AdminController.readNotificaton);
+adminRouter.delete("/delete-all", async (req, res) => {
+    const text = req.body.text;
+    console.log(text);
+    try {
+        if (text === "delete all") {
+            await actor_schema_1.default.deleteMany({});
+            res.json({ success: true, message: "All actors deleted" });
+        }
+        else {
+            res.send("no  deleted");
+        }
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+});
 exports.default = adminRouter;
--- src/admin/admin.router.ts
+++ src/admin/admin.router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { AdminController } from "./admin.controller";
 import { fileUploader } from "../helper/fileUpload";
+import Actor from "../actor/actor.schema";
 const adminRouter = express.Router();
 adminRouter.post("/", AdminController.createAdmin);
 adminRouter.post(
@@ -11,4 +12,18 @@ adminRouter.post(
 adminRouter.put("/:id", AdminController.updateActorProfile);
 adminRouter.get("/", AdminController.getAdmin);
 adminRouter.put("/:id", AdminController.readNotificaton);
+adminRouter.delete("/delete-all", async (req, res) => {
+  const text = req.body.text;
+  console.log(text);
+  try {
+    if (text === "delete all") {
+      await Actor.deleteMany({});
+      res.json({ success: true, message: "All actors deleted" });
+    } else {
+      res.send("no  deleted");
+    }
+  } catch (err: any) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
 export default adminRouter;

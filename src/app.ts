9a0e0ef5d2ef6913c6e84b4cc79dff101c7bfdd4
nurnoby464 <nur@iciclecorporation.com>
2025-12-16 12:19:12 +0600

    origin change

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   console.log("Request method:", req.method);
   console.log("Request origin:", req.headers.origin);
 
-  res.setHeader("Access-Control-Allow-Origin", "https://actors-equity-vfxp.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");

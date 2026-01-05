1c2e0eddb8dce96b06378a01e05237f962b6f058
mohabbatali <mohabbatali@iciclecorporation.com>
2026-01-05 12:20:45 +0600

    fix some

--- dist/index.js
+++ dist/index.js
@@ -10,46 +10,13 @@ exports.default = handler;
 //   await connectDB();   // ensure MongoDB is connected
 //   return app(req, res); // run Express app
 // }
-// import app from "./app";
-// import { connectDB } from "./db";
-// const isVercel = process.env.VERCEL === "1";
-// // --- PRODUCTION: Vercel serverless handler ---
-// export default async function handler(req: any, res: any) {
-//   await connectDB();
-//   return app(req, res);
-// }
-// // --- DEVELOPMENT: local server ---
-// if (!isVercel) {
-//   const port = process.env.PORT || 8000;
-//   const startServer = async () => {
-//     try {
-//       await connectDB();
-//       app.listen(port, () => {
-//         console.log("🚀 Server running on http://localhost:" + port);
-//       });
-//     } catch (err) {
-//       console.error("❌ Failed to start server:", err);
-//     }
-//   };
-//   startServer();
-// }
-// fix for bufferCommands
 const app_1 = __importDefault(require("./app"));
 const db_1 = require("./db");
 const isVercel = process.env.VERCEL === "1";
 // --- PRODUCTION: Vercel serverless handler ---
 async function handler(req, res) {
-    try {
-        await (0, db_1.connectDB)();
-        return (0, app_1.default)(req, res);
-    }
-    catch (error) {
-        console.error("❌ API Handler Error:", error);
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-        });
-    }
+    await (0, db_1.connectDB)();
+    return (0, app_1.default)(req, res);
 }
 // --- DEVELOPMENT: local server ---
 if (!isVercel) {
@@ -63,8 +30,40 @@ if (!isVercel) {
         }
         catch (err) {
             console.error("❌ Failed to start server:", err);
-            process.exit(1);
         }
     };
     startServer();
 }
+// fix for bufferCommands
+// import app from "./app";
+// import { connectDB } from "./db";
+// const isVercel = process.env.VERCEL === "1";
+// // --- PRODUCTION: Vercel serverless handler ---
+// export default async function handler(req: any, res: any) {
+//   try {
+//     await connectDB();
+//     return app(req, res);
+//   } catch (error) {
+//     console.error("❌ API Handler Error:", error);
+//     res.status(500).json({
+//       success: false,
+//       message: "Internal Server Error",
+//     });
+//   }
+// }
+// // --- DEVELOPMENT: local server ---
+// if (!isVercel) {
+//   const port = process.env.PORT || 8000;
+//   const startServer = async () => {
+//     try {
+//       await connectDB();
+//       app.listen(port, () => {
+//         console.log("🚀 Server running on http://localhost:" + port);
+//       });
+//     } catch (err) {
+//       console.error("❌ Failed to start server:", err);
+//       process.exit(1);
+//     }
+//   };
+//   startServer();
+// }

affa1a4596085dedea38e4e9434675e56dbdb46d
nurnoby464 <nur@iciclecorporation.com>
2026-09-08 13:58:57 +0600

    update admin to uesr

--- dist/middleware/verifyAdmin.js
+++ dist/middleware/verifyAdmin.js
@@ -5,15 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VerifyAdmin = void 0;
 const actor_schema_1 = __importDefault(require("../actor/actor.schema"));
-const admin_schema_1 = require("../admin/admin.schema");
 const error_1 = require("./error");
+const user_schema_1 = require("../user/user.schema");
 const VerifyAdmin = async (req, res, next) => {
     const user = req.user;
     const { _id } = user.data;
     const actor = await actor_schema_1.default.findOne({ _id, role: { $in: ["admin"] } });
-    const admin = await admin_schema_1.Admin.findOne({
+    const admin = await user_schema_1.User.findOne({
         _id,
-        role: { $in: ["admin", "superadmin", "moderator"] },
+        role: { $in: ["admin", "superadmin", "moderator", "account"] },
         isActive: true,
     });
     if (!actor && !admin) {
--- src/middleware/verifyAdmin.ts
+++ src/middleware/verifyAdmin.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Actor from "../actor/actor.schema";
 import { Admin } from "../admin/admin.schema";
 import { AppError } from "./error";
+import { User } from "../user/user.schema";
 
 export const VerifyAdmin = async (
   req: Request & { user?: any },
@@ -11,9 +12,9 @@ export const VerifyAdmin = async (
   const user = req.user;
   const { _id } = user.data;
   const actor = await Actor.findOne({ _id, role: { $in: ["admin"] } });
-  const admin = await Admin.findOne({
+  const admin = await User.findOne({
     _id,
-    role: { $in: ["admin", "superadmin", "moderator"] },
+    role: { $in: ["admin", "superadmin", "moderator","account"] },
     isActive: true,
   });
   if (!actor && !admin) {
